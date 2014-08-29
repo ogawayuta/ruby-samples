4ecdd665cdb61f46e803ceb74d2dcd9919e88900
ogawayuta <Og123017@gmail.com>
2014-08-29 14:58:09 +0900

    add file

--- class3.rb
+++ class3.rb
@@ -23,4 +23,46 @@ bob.name = "Bob"
 p bob.name #=> Bob
 
 
+puts "=========="
+class Greeting
+ attr_reader :hello
+ attr_writer :bye
+ attr_accessor :pardon
+end
+greeting = Greeting.new
+greeting.hello #=> nil
+greeting.bye = 42
+greeting.pardon = "sorry"
+p greeting.pardon #=> sorry
 
+
+puts "=========="
+class Person
+# メソッド名の末尾に「=」がないメソッド定義
+ def result(value)
+  @name = value
+ end
+end
+bob = Person.new
+# この形式で値を変更できない
+#bob.result = "Bob"
+#NoMethodError: undefined method 'result=' for #<Person:0xb7bdb520>
+# from (irb):11
+# from :0
+
+
+puts "=========="
+p @foo #=> nil
+p @foo.nil? #=> true
+  @foo = "foo"
+p @foo.nil? #=> false
+
+
+puts "=========="
+text = "abcde "
+# メソッド呼出し
+p text.chop #=> "abcde"
+p text #=> "abcde "
+# 破壊的なメソッド呼出し
+p text.chop! #=> "abcde"
+p text #=> "abcde"
